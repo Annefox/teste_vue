510895bb63e63acdd941543d46b7758841140188
Annefox <annelisefox3@gmail.com>
2023-03-06 16:34:18 -0300

    editando botão.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<Batata mensagem="Clique aqui"/>
+<Batata mensagem="Envie aqui"/>
+<Batata mensagem="Clique aqui"/> 
+
+<input/>
+<input/>
+<input/>
+
+<Texto />
+</template>
+
+<script>
+import Batata from './components/botao.vue';
+import Input from './components/input.vue';
+import Texto from './components/texto.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Batata, Input, Texto
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/botao.vue
@@ -0,0 +1,26 @@
+<template> 
+<button> {{ mensagem }} </button>
+
+</template>
+
+
+
+<script>
+export default {
+
+  name: "botao",
+
+  props:{
+    mensagem: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+
+
+<style>
+
+</style>
